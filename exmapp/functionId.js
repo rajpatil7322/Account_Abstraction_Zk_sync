8f3313cc52dec5bd258221e58a9d0c24978e7714
rajpatil7322 <63945913+rajpatil7322@users.noreply.github.com>
2023-01-29 17:17:24 +0530

    function url added

--- exmapp/functionId.js
+++ exmapp/functionId.js
@@ -1,1 +1,3 @@
-export const functionId="urbsqZAasGML60Rk06UjYYbK6O3WfrS4pfqjwcixWxs"
+export const functionId = "urbsqZAasGML60Rk06UjYYbK6O3WfrS4pfqjwcixWxs"
+
+export const function_url = "https://{urbsqZAasGML60Rk06UjYYbK6O3WfrS4pfqjwcixWxs}.exm.run"
